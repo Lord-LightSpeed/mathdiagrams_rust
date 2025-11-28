2f8a2138bd3fef4e23f257da48723831934cc722
Lord-LightSpeed <adamkyte1@gmail.com>
2025-11-28 21:45:01 +0000

    more trying to learn what is happening
    more pulling from examples, ther will be more of this in the future

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ fn main() {
 fn view(app: &App, frame: Frame) {
     let draw = app.draw();
     let r = app.window_rect();
+    let window = app.main_window();
+    let win = window.rect();
     draw.background().color(GREEN);
+    let crosshair_color = gray(0.5);
 
     for r in r.subdivisions_iter() {
         for r in r.subdivisions_iter() {
@@ -20,6 +23,53 @@ fn view(app: &App, frame: Frame) {
                 draw.arrow().weight(5.0).points(start, end);
             }
         }
+    }
+
+    if let Some(monitor) = window.current_monitor() {
+        let w_scale_factor = window.scale_factor();
+        let m_scale_factor = monitor.scale_factor();
+        let mon_phys = monitor.size();
+        let mon = mon_phys.to_logical(w_scale_factor as f64);
+        let mon_w: f32 = mon.width;
+        let mon_h: f32 = mon.height;
+        let text = format!(
+            "
+        Window size: [{:.0}, {:.0}]
+        Window ratio: {:.2}
+        Window scale factor: {:.2}
+        Monitor size: [{:.0}, {:.0}]
+        Monitor ratio: {:.2}
+        Monitor scale factor: {:.2}
+        ",
+            win.w(),
+            win.h(),
+            win.w() / win.h(),
+            w_scale_factor,
+            mon_w,
+            mon_h,
+            mon_w / mon_h,
+            m_scale_factor
+        );
+        let pad = 6.0;
+        draw.text(&text)
+            .h(win.pad(pad).h())
+            .w(win.pad(pad).w())
+            .line_spacing(pad)
+            .font_size(14)
+            .align_text_bottom()
+            .color(crosshair_color)
+            .left_justify();
+
+        // Ellipse at mouse.
+        draw.ellipse().wh([5.0; 2].into()).xy(app.mouse.position());
+
+        // Mouse position text.
+        let mouse = app.mouse.position();
+        let pos = format!("[{:.1}, {:.1}]", mouse.x, mouse.y);
+        draw.text(&pos)
+            .xy(mouse + vec2(0.0, 20.0))
+            .font_size(14)
+            .color(WHITE);
     }
 
     draw.to_frame(app, &frame).unwrap();
